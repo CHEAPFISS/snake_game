79344d00a2ce373d6901bce8a97d4e5d3a8d02dd
CHEAPFISS <forgpt20@gmail.com>
2026-07-19 06:46:32 +0300

    Изменил главный ффайл чтоб довабить описание на crates.io

--- src/game_loop.rs
+++ src/game_loop.rs
@@ -1,44 +1,8 @@
 //! # Game Loop для игры в змейку.
 //!
 //! Имеет в себе функции для запуска и создание игрового цикла.
-//!
-//! Ниже приведен пример для запуска игры:
-//!
-//! # Example
-//! ```rust, no_run
-//! use color_eyre::eyre::Result;
-//! use snake_game::{EventHandler, Game, GameLoop, Snake, Tui};
-//! use std::io::stderr;
-//!
-//! use ratatui::Terminal;
-//!
-//! fn main() -> Result<(), color_eyre::eyre::Report> {
-//!     color_eyre::install()?;
-//!
-//!     let backend = ratatui::backend::CrosstermBackend::new(stderr());
-//!     let terminal = Terminal::new(backend)?;
-//!     let term_size = terminal.size()?;
-//!
-//!     let game = Game::new(
-//!         term_size,
-//!         Snake::default_snake(),
-//!         String::from("Snake Game"),
-//!     );
-//!
-//!     let event_handler = EventHandler::new(100);
-//!     let tui = Tui::new(terminal);
-//!
-//!     let mut game_loop = GameLoop::new(game, event_handler, tui);
-//!     game_loop.run()?;
-//!     Ok(())
-//! }
-//! ```
-//!
-//! # Panics
-//!
-//! - Не удалось установить panic hook.
-//! - Не удалось запустить терминал.
-//! - Не удалось запустить игровой цикл.
+
+
 
 use crate::event::{Event as EventType, EventHandler};
 use crate::game::{Game, snake::SnakeDirection};
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,45 @@
+//! # Snake Game
+//!
+//!
+//! ## Мой личный мини проектик для того, чтоб понять, что нужно для игрового движка на базе [crosster] и [ratatui].
+//!
+//! # Ниже приведен пример для запуска игры:
+//!
+//! # Example
+//! ```rust, no_run
+//! use color_eyre::eyre::Result;
+//! use snake_game::{EventHandler, Game, GameLoop, Snake, Tui};
+//! use std::io::stderr;
+//!
+//! use ratatui::Terminal;
+//!
+//! fn main() -> Result<(), color_eyre::eyre::Report> {
+//!     color_eyre::install()?;
+//!
+//!     let backend = ratatui::backend::CrosstermBackend::new(stderr());
+//!     let terminal = Terminal::new(backend)?;
+//!     let term_size = terminal.size()?;
+//!
+//!     let game = Game::new(
+//!         term_size,
+//!         Snake::default_snake(),
+//!         String::from("Snake Game"),
+//!     );
+//!
+//!     let event_handler = EventHandler::new(100);
+//!     let tui = Tui::new(terminal);
+//!
+//!     let mut game_loop = GameLoop::new(game, event_handler, tui);
+//!     game_loop.run()?;
+//!     Ok(())
+//! }
+//! ```
+//! # Panics
+//!
+//! - Не удалось установить panic hook.
+//! - Не удалось запустить терминал.
+//! - Не удалось запустить игровой цикл.
+
 mod event;
 mod game;
 mod game_loop;
