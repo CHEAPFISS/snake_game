4d08a0c5e4091f13fced82fe596bd1da499bc92e
CHEAPFISS <forgpt20@gmail.com>
2026-07-23 18:12:10 +0300

    full update of ui with pop up menu and 3 new functions (now without commits)

--- src/ui.rs
+++ src/ui.rs
@@ -90,5 +90,95 @@ pub(crate) fn render(game: &mut Game, frame: &mut Frame) {
                 ),
         info_area[2]);
 
+    if game.game_state == GameState::Pause{
+        render_popup_block(frame, &game.menus[&GameState::Pause], &chunks[1]);
+    }
+
+}
+
+fn centered_rect(percent_x: u16, percent_y: u16, r: Rect) -> Rect {
+    // Cut the given rectangle into three vertical pieces
+    let popup_layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([
+            Constraint::Percentage((100 - percent_y) / 2),
+            Constraint::Percentage(percent_y),
+            Constraint::Percentage((100 - percent_y) / 2),
+        ])
+        .split(r);
+
+    // Then cut the middle vertical piece into three width-wise pieces
+    Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([
+            Constraint::Percentage((100 - percent_x) / 2),
+            Constraint::Percentage(percent_x),
+            Constraint::Percentage((100 - percent_x) / 2),
+        ])
+        .split(popup_layout[1])[1] // Return the middle chunk
+}
+
+fn menu_buttons_place_rect(chunk: Rect, items: &[Item]) -> Vec<Rect> {
+    let rect = Layout::default()
+        .direction(Direction::Horizontal)
+        .flex(Flex::SpaceBetween)
+        .constraints(
+            items.iter().map(|item|{
+                Constraint::Min(item.name.len() as u16)
+            })
+        )
+        .split(chunk);
+    rect.to_vec()
+}
+
+fn render_popup_block(frame: &mut Frame, menu: &Menu, chunk: &Rect){
+    let popup_area = centered_rect(40, 30, *chunk);
+
+    let popup_chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([
+            Constraint::Percentage(100),
+            Constraint::Min(2),
+        ])
+        .split(popup_area);
+
+
+    frame.render_widget(Clear, popup_area);
+
+    let popup_block = Block::bordered()
+        .border_style(Style::new()
+            .fg(Color::Cyan))
+        .border_type(BorderType::Plain)
+        .title(Line::from(menu.title.clone())
+            .alignment(Alignment::Center)
+            .style(
+                Style::new()
+                    .add_modifier(Modifier::BOLD)
+                    .fg(Color::LightBlue)));
+
+    frame.render_widget(popup_block, popup_area);
+
+
+    let buttons_chunk = menu_buttons_place_rect(popup_chunks[1], &menu.items);
+
+    for (index, item) in menu.items.iter().enumerate() {
+        let style = if index == menu.selected {
+            Style::new().fg(Color::DarkGray).bg(Color::Yellow)
+        } else {
+            Style::new().fg(Color::Gray)
+        };
+
+
+        let text = Span::from(item.name.as_str())
+            .style(style);
+
+        frame.render_widget(
+            Paragraph::new(text)
+                .alignment(Alignment::Center),
+            buttons_chunk[index]
+        );
+    }
+
+
 
 }
